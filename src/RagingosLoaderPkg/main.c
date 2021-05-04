10a1a76ce5618adf9f3ad041b53e3b8e98f84624
ragingo <save.the.world.crush@gmail.com>
2021-05-04 21:41:33 +0900

    コードフォーマット

--- src/RagingosLoaderPkg/main.c
+++ src/RagingosLoaderPkg/main.c
@@ -9,25 +9,23 @@
 
 struct MemoryMap {
     UINTN buffer_size;
-    VOID *buffer;
+    VOID* buffer;
     UINTN map_size;
     UINTN map_key;
     UINTN descriptor_size;
     UINT32 descriptor_version;
 };
 
-EFI_STATUS GetMemoryMap(struct MemoryMap *map) {
+EFI_STATUS GetMemoryMap(struct MemoryMap* map) {
     if (map->buffer == NULL) {
         return EFI_BUFFER_TOO_SMALL;
     }
 
     map->map_size = map->buffer_size;
-    return gBS->GetMemoryMap(
-        &map->map_size, (EFI_MEMORY_DESCRIPTOR *)map->buffer, &map->map_key,
-        &map->descriptor_size, &map->descriptor_version);
+    return gBS->GetMemoryMap(&map->map_size, (EFI_MEMORY_DESCRIPTOR*)map->buffer, &map->map_key, &map->descriptor_size, &map->descriptor_version);
 }
 
-const CHAR16 *GetMemoryTypeUnicode(EFI_MEMORY_TYPE type) {
+const CHAR16* GetMemoryTypeUnicode(EFI_MEMORY_TYPE type) {
     switch (type) {
     case EfiReservedMemoryType:
         return L"EfiReservedMemoryType";
@@ -66,72 +64,60 @@ const CHAR16 *GetMemoryTypeUnicode(EFI_MEMORY_TYPE type) {
     }
 }
 
-EFI_STATUS SaveMemoryMap(struct MemoryMap *map, EFI_FILE_PROTOCOL *file) {
+EFI_STATUS SaveMemoryMap(struct MemoryMap* map, EFI_FILE_PROTOCOL* file) {
     CHAR8 buf[256];
     UINTN len;
 
-    CHAR8 *header =
-        "Index, Type, Type(name), PhysicalStart, NumberOfPages, Attribute\n";
+    CHAR8* header = "Index, Type, Type(name), PhysicalStart, NumberOfPages, Attribute\n";
     len = AsciiStrLen(header);
     file->Write(file, &len, header);
 
-    Print(L"map->buffer = %08lx, map->map_size = %08lx\n", map->buffer,
-          map->map_size);
+    Print(L"map->buffer = %08lx, map->map_size = %08lx\n", map->buffer, map->map_size);
 
     EFI_PHYSICAL_ADDRESS iter;
     int i;
-    for (iter = (EFI_PHYSICAL_ADDRESS)map->buffer, i = 0;
-         iter < (EFI_PHYSICAL_ADDRESS)map->buffer + map->map_size;
-         iter += map->descriptor_size, i++) {
-        EFI_MEMORY_DESCRIPTOR *desc = (EFI_MEMORY_DESCRIPTOR *)iter;
-        len = AsciiSPrint(buf, sizeof(buf), "%u, %x, %-ls, %08lx, %lx, %lx\n",
-                          i, desc->Type, GetMemoryTypeUnicode(desc->Type),
-                          desc->PhysicalStart, desc->NumberOfPages,
-                          desc->Attribute & 0xffffflu);
+    for (iter = (EFI_PHYSICAL_ADDRESS)map->buffer, i = 0; iter < (EFI_PHYSICAL_ADDRESS)map->buffer + map->map_size; iter += map->descriptor_size, i++) {
+        EFI_MEMORY_DESCRIPTOR* desc = (EFI_MEMORY_DESCRIPTOR*)iter;
+        len = AsciiSPrint(buf, sizeof(buf), "%u, %x, %-ls, %08lx, %lx, %lx\n", i, desc->Type, GetMemoryTypeUnicode(desc->Type), desc->PhysicalStart, desc->NumberOfPages, desc->Attribute & 0xffffflu);
         file->Write(file, &len, buf);
     }
 
     return EFI_SUCCESS;
 }
 
-EFI_STATUS OpenRootDir(EFI_HANDLE image_handle, EFI_FILE_PROTOCOL **root) {
-    EFI_LOADED_IMAGE_PROTOCOL *loaded_image;
-    EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *fs;
+EFI_STATUS OpenRootDir(EFI_HANDLE image_handle, EFI_FILE_PROTOCOL** root) {
+    EFI_LOADED_IMAGE_PROTOCOL* loaded_image;
+    EFI_SIMPLE_FILE_SYSTEM_PROTOCOL* fs;
 
-    gBS->OpenProtocol(image_handle, &gEfiLoadedImageProtocolGuid,
-                      (VOID **)&loaded_image, image_handle, NULL,
-                      EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+    gBS->OpenProtocol(image_handle, &gEfiLoadedImageProtocolGuid, (VOID**)&loaded_image, image_handle, NULL, EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
 
-    gBS->OpenProtocol(loaded_image->DeviceHandle,
-                      &gEfiSimpleFileSystemProtocolGuid, (VOID **)&fs,
-                      image_handle, NULL, EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+    gBS->OpenProtocol(loaded_image->DeviceHandle, &gEfiSimpleFileSystemProtocolGuid, (VOID**)&fs, image_handle, NULL, EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
 
     fs->OpenVolume(fs, root);
 
     return EFI_SUCCESS;
 }
 
-EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle,
-                           EFI_SYSTEM_TABLE *system_table) {
+EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE* system_table) {
     Print(L"Hello, Mikan World!\n");
 
     CHAR8 memmap_buf[4096 * 4];
-    struct MemoryMap memmap = {sizeof(memmap_buf), memmap_buf, 0, 0, 0, 0};
+    struct MemoryMap memmap = { sizeof(memmap_buf), memmap_buf, 0, 0, 0, 0 };
     GetMemoryMap(&memmap);
 
-    EFI_FILE_PROTOCOL *root_dir;
+    EFI_FILE_PROTOCOL* root_dir;
     OpenRootDir(image_handle, &root_dir);
 
-    EFI_FILE_PROTOCOL *memmap_file;
-    root_dir->Open(
-        root_dir, &memmap_file, L"\\memmap",
-        EFI_FILE_MODE_READ | EFI_FILE_MODE_WRITE | EFI_FILE_MODE_CREATE, 0);
+    EFI_FILE_PROTOCOL* memmap_file;
+    root_dir->Open(root_dir, &memmap_file, L"\\memmap", EFI_FILE_MODE_READ | EFI_FILE_MODE_WRITE | EFI_FILE_MODE_CREATE, 0);
 
     SaveMemoryMap(&memmap, memmap_file);
     memmap_file->Close(memmap_file);
 
     Print(L"All done\n");
 
-    while (1);
+    while (1) {
+    }
+
     return EFI_SUCCESS;
 }
