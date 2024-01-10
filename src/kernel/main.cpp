0656e44adb3024d20e2b54c877d82cf42d59b212
ragingo <save.the.world.crush@gmail.com>
2024-01-11 00:23:28 +0900

    osbook_day05a フォント描画

--- src/kernel/main.cpp
+++ src/kernel/main.cpp
@@ -1,7 +1,34 @@
-#include <cstddef>
+/**
+ * @file main.cpp
+ *
+ * カーネル本体のプログラムを書いたファイル．
+ */
+
 #include <cstdint>
+#include <cstddef>
 
 #include "frame_buffer_config.hpp"
+
+// #@@range_begin(font_a)
+const uint8_t kFontA[16] = {
+    0b00000000,  //
+    0b00011000,  //    **
+    0b00011000,  //    **
+    0b00011000,  //    **
+    0b00011000,  //    **
+    0b00100100,  //   *  *
+    0b00100100,  //   *  *
+    0b00100100,  //   *  *
+    0b00100100,  //   *  *
+    0b01111110,  //  ******
+    0b01000010,  //  *    *
+    0b01000010,  //  *    *
+    0b01000010,  //  *    *
+    0b11100111,  // ***  ***
+    0b00000000,  //
+    0b00000000,  //
+};
+// #@@range_end(font_a)
 
 struct PixelColor {
     uint8_t r, g, b;
@@ -28,7 +55,7 @@ class RGBResv8BitPerColorPixelWriter : public PixelWriter {
 public:
     using PixelWriter::PixelWriter;
 
-    void Write(int x, int y, const PixelColor& c) override {
+    virtual void Write(int x, int y, const PixelColor& c) override {
         auto p = PixelAt(x, y);
         p[0] = c.r;
         p[1] = c.g;
@@ -40,13 +67,28 @@ class BGRResv8BitPerColorPixelWriter : public PixelWriter {
 public:
     using PixelWriter::PixelWriter;
 
-    void Write(int x, int y, const PixelColor& c) override {
+    virtual void Write(int x, int y, const PixelColor& c) override {
         auto p = PixelAt(x, y);
         p[0] = c.b;
         p[1] = c.g;
         p[2] = c.r;
     }
 };
+
+// #@@range_begin(write_ascii)
+void WriteAscii(PixelWriter& writer, int x, int y, char c, const PixelColor& color) {
+    if (c != 'A') {
+        return;
+    }
+    for (int dy = 0; dy < 16; ++dy) {
+        for (int dx = 0; dx < 8; ++dx) {
+            if ((kFontA[dy] << dx) & 0x80u) {
+                writer.Write(x + dx, y + dy, color);
+            }
+        }
+    }
+}
+// #@@range_end(write_ascii)
 
 void* operator new(size_t size, void* buf) {
     return buf;
@@ -56,7 +98,7 @@ void operator delete(void* obj) noexcept {
 }
 
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
-PixelWriter* pixel_writer = nullptr;
+PixelWriter* pixel_writer;
 
 extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
     switch (frame_buffer_config.pixel_format) {
@@ -78,7 +120,10 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config) {
             pixel_writer->Write(x, y, { 0, 255, 0 });
         }
     }
-    while (1) {
-        __asm__("hlt");
-    }
+
+    // #@@range_begin(write_aa)
+    WriteAscii(*pixel_writer, 50, 50, 'A', { 0, 0, 0 });
+    WriteAscii(*pixel_writer, 58, 50, 'A', { 0, 0, 0 });
+    // #@@range_end(write_aa)
+    while (1) __asm__("hlt");
 }
