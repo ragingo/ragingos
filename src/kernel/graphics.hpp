88dfed0052314d9e6bb5152a9b062d4990e6beb8
ragingo <save.the.world.crush@gmail.com>
2024-01-13 23:17:13 +0900

    osbook_day06a マウスカーソルとデスクトップの描画

--- src/kernel/graphics.hpp
+++ src/kernel/graphics.hpp
@@ -1,12 +1,10 @@
 #pragma once
 
-// #@@range_begin(pixel_color_def)
 #include "frame_buffer_config.hpp"
 
 struct PixelColor {
     uint8_t r, g, b;
 };
-// #@@range_end(pixel_color_def)
 
 class PixelWriter {
 public:
@@ -25,7 +23,6 @@ private:
     const FrameBufferConfig& config_;
 };
 
-// #@@range_begin(pixel_writer_def)
 class RGBResv8BitPerColorPixelWriter : public PixelWriter {
 public:
     using PixelWriter::PixelWriter;
@@ -37,4 +34,16 @@ public:
     using PixelWriter::PixelWriter;
     virtual void Write(int x, int y, const PixelColor& c) override;
 };
-// #@@range_end(pixel_writer_def)
+
+// #@@range_begin(vector2d)
+template<typename T>
+struct Vector2D {
+    T x, y;
+};
+// #@@range_end(vector2d)
+
+void DrawRectangle(PixelWriter& writer, const Vector2D<int>& pos,
+                   const Vector2D<int>& size, const PixelColor& c);
+
+void FillRectangle(PixelWriter& writer, const Vector2D<int>& pos,
+                   const Vector2D<int>& size, const PixelColor& c);
