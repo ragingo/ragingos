986e777012ff2b8e8aa32493790ef957c58c4c33
ragingo <save.the.world.crush@gmail.com>
2024-01-16 00:11:50 +0900

    osbook_day06c マウスが動作するようQEMUのオプションを追加

--- scripts/run_disk.sh
+++ scripts/run_disk.sh
@@ -19,4 +19,7 @@ qemu-system-x86_64 \
   -drive if=pflash,format=raw,readonly=on,file=$OUTPUT_PATH/OVMF_CODE.fd \
   -drive if=pflash,format=raw,file=$OUTPUT_PATH/OVMF_VARS.fd \
   -drive if=ide,index=0,media=disk,format=raw,file=$OUTPUT_PATH/disk.img \
+  -device nec-usb-xhci,id=xhci \
+  -device usb-mouse \
+  -device usb-kbd \
   -monitor stdio
