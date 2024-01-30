d81ae6fbc5a8abe31091889c8a876789b268807d
ragingo <save.the.world.crush@gmail.com>
2024-01-30 14:33:21 +0900

    osbook_day11c タイマの分解能を上げる

--- src/kernel/interrupt.cpp
+++ src/kernel/interrupt.cpp
@@ -8,6 +8,7 @@
 
 #include "asmfunc.h"
 #include "segment.hpp"
+#include "timer.hpp"
 
 std::array<InterruptDescriptor, 256> idt;
 
@@ -36,7 +37,7 @@ namespace {
     }
 
     __attribute__((interrupt)) void IntHandlerLAPICTimer(InterruptFrame* frame) {
-        msg_queue->push_back(Message { Message::kInterruptLAPICTimer });
+        LAPICTimerOnInterrupt();
         NotifyEndOfInterrupt();
     }
 }  // namespace
