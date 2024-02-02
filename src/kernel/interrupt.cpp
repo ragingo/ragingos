c53b048751ca1b2ac9e0214e449fa070e1dd667a
ragingo <save.the.world.crush@gmail.com>
2024-02-03 00:00:59 +0900

    osbook_day13b コンテキストスイッチの自動化

--- src/kernel/interrupt.cpp
+++ src/kernel/interrupt.cpp
@@ -38,7 +38,6 @@ namespace {
 
     __attribute__((interrupt)) void IntHandlerLAPICTimer(InterruptFrame* frame) {
         LAPICTimerOnInterrupt();
-        NotifyEndOfInterrupt();
     }
 }  // namespace
 
