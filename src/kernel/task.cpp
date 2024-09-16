8127f3d5d25370c2e28bb03b0c858030b332e28f
ragingo <save.the.world.crush@gmail.com>
2024-09-16 22:15:09 +0900

    osbook_day23c タイマーアプリ

--- src/kernel/task.cpp
+++ src/kernel/task.cpp
@@ -239,7 +239,7 @@ void InitializeTask() {
 
     __asm__("cli");
     timer_manager->AddTimer(
-        Timer { timer_manager->CurrentTick() + kTaskTimerPeriod, kTaskTimerValue });
+        Timer { timer_manager->CurrentTick() + kTaskTimerPeriod, kTaskTimerValue, 1 });
     __asm__("sti");
 }
 
