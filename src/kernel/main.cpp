22bf486f050de71233c6c24c3236217612c2a1dc
ragingo <save.the.world.crush@gmail.com>
2024-09-26 21:43:21 +0900

    ターミナル以外の初期表示されるウィンドウを削除

--- src/kernel/main.cpp
+++ src/kernel/main.cpp
@@ -52,71 +52,6 @@ __attribute__((format(printf, 1, 2))) int printk(const char* format, ...) {
     return result;
 }
 
-std::shared_ptr<ToplevelWindow> main_window;
-unsigned int main_window_layer_id;
-void InitializeMainWindow() {
-    main_window = std::make_shared<ToplevelWindow>(
-        160, 52, screen_config.pixel_format, "Hello Window");
-
-    main_window_layer_id = layer_manager->NewLayer()
-                               .SetWindow(main_window)
-                               .SetDraggable(true)
-                               .Move({ 300, 100 })
-                               .ID();
-
-    layer_manager->UpDown(main_window_layer_id, std::numeric_limits<int>::max());
-}
-
-std::shared_ptr<ToplevelWindow> text_window;
-unsigned int text_window_layer_id;
-void InitializeTextWindow() {
-    const int win_w = 160;
-    const int win_h = 52;
-
-    text_window = std::make_shared<ToplevelWindow>(
-        win_w, win_h, screen_config.pixel_format, "Text Box Test");
-    DrawTextbox(*text_window->InnerWriter(), { 0, 0 }, text_window->InnerSize());
-
-    text_window_layer_id = layer_manager->NewLayer()
-                               .SetWindow(text_window)
-                               .SetDraggable(true)
-                               .Move({ 500, 100 })
-                               .ID();
-
-    layer_manager->UpDown(text_window_layer_id, std::numeric_limits<int>::max());
-}
-
-int text_window_index;
-
-void DrawTextCursor(bool visible) {
-    const auto color = visible ? ToColor(0) : ToColor(0xffffff);
-    const auto pos = Vector2D<int> { 4 + 8 * text_window_index, 5 };
-    FillRectangle(*text_window->InnerWriter(), pos, { 7, 15 }, color);
-}
-
-void InputTextWindow(char c) {
-    if (c == 0) {
-        return;
-    }
-
-    auto pos = []() { return Vector2D<int> { 4 + 8 * text_window_index, 6 }; };
-
-    const int max_chars = (text_window->InnerSize().x - 8) / 8 - 1;
-    if (c == '\b' && text_window_index > 0) {
-        DrawTextCursor(false);
-        --text_window_index;
-        FillRectangle(*text_window->InnerWriter(), pos(), { 8, 16 }, ToColor(0xffffff));
-        DrawTextCursor(true);
-    } else if (c >= ' ' && text_window_index < max_chars) {
-        DrawTextCursor(false);
-        WriteAscii(*text_window->InnerWriter(), pos(), c, ToColor(0));
-        ++text_window_index;
-        DrawTextCursor(true);
-    }
-
-    layer_manager->Draw(text_window_layer_id);
-}
-
 alignas(16) uint8_t kernel_main_stack[1024 * 1024];
 
 // デスクトップの右下（タスクバーの右端）に現在時刻を表示する
@@ -203,17 +138,10 @@ extern "C" void KernelMainNewStack(
     InitializePCI();
 
     InitializeLayer();
-    InitializeMainWindow();
-    InitializeTextWindow();
     layer_manager->Draw({ { 0, 0 }, ScreenSize() });
 
     acpi::Initialize(acpi_table);
     InitializeLAPICTimer();
-
-    const int kTextboxCursorTimer = 1;
-    const int kTimer05Sec = static_cast<int>(kTimerFreq * 0.5);
-    timer_manager->AddTimer(Timer { kTimer05Sec, kTextboxCursorTimer, 1 });
-    bool textbox_cursor_visible = false;
 
     InitializeSyscall();
 
@@ -237,15 +165,6 @@ extern "C" void KernelMainNewStack(
 
     while (true) {
         __asm__("cli");
-        const auto tick = timer_manager->CurrentTick();
-        __asm__("sti");
-
-        sprintf(str, "%010lu", tick);
-        FillRectangle(*main_window->InnerWriter(), { 20, 4 }, { 8 * 10, 16 }, { 0xc6, 0xc6, 0xc6 });
-        WriteString(*main_window->InnerWriter(), { 20, 4 }, str, { 0, 0, 0 });
-        layer_manager->Draw(main_window_layer_id);
-
-        __asm__("cli");
         auto msg = main_task.ReceiveMessage();
         if (!msg) {
             main_task.Sleep();
@@ -260,27 +179,14 @@ extern "C" void KernelMainNewStack(
             usb::xhci::ProcessEvents();
             break;
         case Message::kTimerTimeout:
-            if (msg->arg.timer.value == kTextboxCursorTimer) {
-                __asm__("cli");
-                timer_manager->AddTimer(
-                    Timer { msg->arg.timer.timeout + kTimer05Sec, kTextboxCursorTimer, 1 });
-                __asm__("sti");
-                textbox_cursor_visible = !textbox_cursor_visible;
-                DrawTextCursor(textbox_cursor_visible);
-                layer_manager->Draw(text_window_layer_id);
-            }
             break;
         case Message::kKeyPush:
-            if (auto act = active_layer->GetActive(); act == text_window_layer_id) {
-                if (msg->arg.keyboard.press) {
-                    InputTextWindow(msg->arg.keyboard.ascii);
-                }
-            } else if (msg->arg.keyboard.press &&
-                       msg->arg.keyboard.keycode == 59 /* F2 */) {
+            if (msg->arg.keyboard.press && msg->arg.keyboard.keycode == 59 /* F2 */) {
                 task_manager->NewTask()
                     .InitContext(TaskTerminal, 0)
                     .Wakeup();
             } else {
+                auto act = active_layer->GetActive();
                 __asm__("cli");
                 auto task_it = layer_task_map->find(act);
                 __asm__("sti");
