3bb0b9b16208c044067bf986eece972404f767b2
ragingo <save.the.world.crush@gmail.com>
2024-09-26 15:24:17 +0900

    osbook_day30f 画像ビューア作成

--- scripts/formatter.sh
+++ scripts/formatter.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 
 (git diff --name-only --diff-filter=AM && git diff --name-only --diff-filter=AM --cached) | \
+    grep -v "lib/" | \
     grep -E "\.(h|hpp|c|cpp)$" | \
     xargs -i% clang-format -i % -style=file
