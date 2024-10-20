9c6be5a18712fe898b8b6f08604fd5305fa17467
ragingo <4784032+ragingo@users.noreply.github.com>
2024-10-20 23:40:00 +0900

    EDK2 OvmfPkg ビルド対応

--- scripts/build_edk2.sh
+++ scripts/build_edk2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
-source ./scripts/buildenv.sh
+EDK2_HOME=$(realpath -m ./lib/edk2)
 
 if [ ! -d $EDK2_HOME ]; then
   mkdir -p $EDK2_HOME
@@ -8,21 +8,34 @@ if [ ! -d $EDK2_HOME ]; then
   pushd $EDK2_HOME
   git fetch --depth=1 --tags
   git checkout tags/edk2-stable202408
-  # BaseTools/Source/C のビルドを通すため、 BaseTools/Source/C/BrotliCompress/brotli を取得
+  # BaseTools/Source/C ビルド用
   git submodule update --init --depth=1 BaseTools/Source/C/BrotliCompress/brotli
-  # RagingosLoaderPkg のビルドを通すため、 MdePkg/Library/MipiSysTLib/mipisyst を取得
+  # RagingosLoaderPkg ビルド用
   git submodule update --init --depth=1 MdePkg/Library/MipiSysTLib/mipisyst
+  # OvmfPkg ビルド用
+  git submodule update --init --depth=1 MdeModulePkg/Library/BrotliCustomDecompressLib/brotli
+  git submodule update --init --depth=1 CryptoPkg/Library/OpensslLib/openssl
+  git submodule update --init --depth=1 CryptoPkg/Library/MbedTlsLib/mbedtls
+  git submodule update --init --depth=1 SecurityPkg/DeviceSecurity/SpdmLib/libspdm
   make -C BaseTools/Source/C
   popd
 fi
 
-ln -fs $(realpath ./src/RagingosLoaderPkg) $EDK2_HOME/
-cp -p ./setup/edk2/target.txt $EDK2_HOME/Conf/target.txt
 cp -p ./setup/edk2/tools_def.txt $EDK2_HOME/Conf/tools_def.txt
 
-pushd $EDK2_HOME
+# OvmfPkg のビルド
+cp -p ./setup/edk2/target_OvmfPkg.txt $EDK2_HOME/Conf/target.txt
 
+pushd $EDK2_HOME
 source ./edksetup.sh
 build
+popd
 
+# RagingosLoaderPkg のビルド
+ln -fs $(realpath ./src/RagingosLoaderPkg) $EDK2_HOME/
+cp -p ./setup/edk2/target_RagingosLoaderPkg.txt $EDK2_HOME/Conf/target.txt
+
+pushd $EDK2_HOME
+source ./edksetup.sh
+build
 popd
