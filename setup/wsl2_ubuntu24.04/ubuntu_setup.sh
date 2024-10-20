3686cd5b9e9c41e08a9b8d275ae0441fc4459bc7
ragingo <4784032+ragingo@users.noreply.github.com>
2024-10-20 14:44:07 +0900

    Ubuntu 24.04 向けセットアップスクリプトを修正

--- setup/wsl2_ubuntu24.04/ubuntu_setup.sh
+++ setup/wsl2_ubuntu24.04/ubuntu_setup.sh
@@ -3,5 +3,10 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install -y ansible
+
+curl -fsSL https://apt.llvm.org/llvm-snapshot.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/llvm.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/llvm.gpg] http://apt.llvm.org/noble/ llvm-toolchain-noble main" | sudo tee /etc/apt/sources.list.d/llvm.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/llvm.gpg] http://apt.llvm.org/noble/ llvm-toolchain-noble-19 main" | sudo tee -a /etc/apt/sources.list.d/llvm.list > /dev/null
+
 ansible-playbook -K -i ansible_inventory ansible_provision.yml
 iasl -v
