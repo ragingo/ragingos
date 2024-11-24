3d2fb39bdbb3290b8b2e9441e9fa2138c49a2285
ragingo <4784032+ragingo@users.noreply.github.com>
2024-11-24 22:55:29 +0900

    OS起動スクリプト修正

--- scripts/run_os_disk.sh
+++ scripts/run_os_disk.sh
@@ -5,22 +5,33 @@ readonly OUTPUT_PATH=./build
 
 pushd $OUTPUT_PATH > /dev/null
 
-if [[ ! -e ./OVMF_CODE.fd ]]; then
-  curl -OL https://github.com/uchan-nos/mikanos-build/raw/master/devenv/OVMF_CODE.fd
-fi
-if [[ ! -e ./OVMF_VARS.fd ]]; then
-  curl -OL https://github.com/uchan-nos/mikanos-build/raw/master/devenv/OVMF_VARS.fd
-fi
+cp ../lib/edk2/Build/OvmfX64/DEBUG_GCC5/FV/OVMF.fd OVMF_SELF_BUILD.fd
+cp /usr/share/ovmf/OVMF.fd OVMF_Ubuntu24-apt.fd
 
 popd > /dev/null
 
+# FIRMWARE=""
+# FIRMWARE="-bios $OUTPUT_PATH/DEBUGX64_OVMF.fd"
+# FIRMWARE="-bios $OUTPUT_PATH/OVMF_Ubuntu22-apt.fd"
+# FIRMWARE="-bios $OUTPUT_PATH/OVMF_Ubuntu24-apt.fd"
+# FIRMWARE="-bios $OUTPUT_PATH/OVMF_SELF_BUILD.fd"
+# FIRMWARE="\
+#   -drive if=pflash,format=raw,readonly=on,file=$OUTPUT_PATH/OSBOOK_OVMF_CODE.fd \
+#   -drive if=pflash,format=raw,file=$OUTPUT_PATH/OSBOOK_OVMF_VARS.fd
+# "
+
+QEMU_OPTS=""
+# QEMU_OPTS="-gdb tcp::12345 -S"
+QEMU_OPTS="-debugcon file:debug.log -global isa-debugcon.iobase=0x402 -d int -D qemu.log"
+
 qemu-system-x86_64 \
-  -m 1G \
-  -drive if=pflash,format=raw,readonly=on,file=$OUTPUT_PATH/OVMF_CODE.fd \
-  -drive if=pflash,format=raw,file=$OUTPUT_PATH/OVMF_VARS.fd \
+  $FIRMWARE \
+  -device qemu-xhci \
+  -m 2G \
   -drive if=ide,index=0,media=disk,format=raw,file=$OUTPUT_PATH/disk.img \
-  -device nec-usb-xhci,id=xhci \
-  -device usb-mouse \
   -device usb-kbd \
-  -device usb-tablet \
-  -monitor stdio
+  -device usb-tablet,usb_version=1 \
+  -monitor stdio \
+  --no-reboot \
+  --no-shutdown \
+  $QEMU_OPTS
