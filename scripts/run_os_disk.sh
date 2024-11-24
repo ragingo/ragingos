1ba22d0523f3f1d08843e03f7c4a576430dcebb3
ragingo <4784032+ragingo@users.noreply.github.com>
2024-11-25 00:02:57 +0900

    OS起動スクリプト修正

--- scripts/run_os_disk.sh
+++ scripts/run_os_disk.sh
@@ -11,14 +11,14 @@ cp /usr/share/ovmf/OVMF.fd OVMF_Ubuntu24-apt.fd
 popd > /dev/null
 
 # FIRMWARE=""
-# FIRMWARE="-bios $OUTPUT_PATH/DEBUGX64_OVMF.fd"
-# FIRMWARE="-bios $OUTPUT_PATH/OVMF_Ubuntu22-apt.fd"
-# FIRMWARE="-bios $OUTPUT_PATH/OVMF_Ubuntu24-apt.fd"
-# FIRMWARE="-bios $OUTPUT_PATH/OVMF_SELF_BUILD.fd"
+FIRMWARE="-bios $OUTPUT_PATH/OVMF_Ubuntu22-apt.fd" # タスクバーは消える以外は問題なさそう
+# FIRMWARE="-bios $OUTPUT_PATH/OVMF_Ubuntu24-apt.fd" # 起動直後にOS停止
+# FIRMWARE="-bios $OUTPUT_PATH/OVMF_SELF_BUILD.fd" # 起動直後にOS停止
+# FIRMWARE="-bios $OUTPUT_PATH/DEBUGX64_OVMF.fd" # 起動直後にOS停止
 # FIRMWARE="\
 #   -drive if=pflash,format=raw,readonly=on,file=$OUTPUT_PATH/OSBOOK_OVMF_CODE.fd \
 #   -drive if=pflash,format=raw,file=$OUTPUT_PATH/OSBOOK_OVMF_VARS.fd
-# "
+# " # タスクバーは消える以外は問題なさそう
 
 QEMU_OPTS=""
 # QEMU_OPTS="-gdb tcp::12345 -S"
@@ -26,11 +26,12 @@ QEMU_OPTS="-debugcon file:debug.log -global isa-debugcon.iobase=0x402 -d int -D 
 
 qemu-system-x86_64 \
   $FIRMWARE \
-  -device qemu-xhci \
   -m 2G \
   -drive if=ide,index=0,media=disk,format=raw,file=$OUTPUT_PATH/disk.img \
+  -device nec-usb-xhci,id=xhci \
+  -device usb-mouse \
   -device usb-kbd \
-  -device usb-tablet,usb_version=1 \
+  -device usb-tablet \
   -monitor stdio \
   --no-reboot \
   --no-shutdown \
