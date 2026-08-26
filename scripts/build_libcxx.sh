f2b23228a0783301f8f7a26de8281963ac0f2a77
ragingo <4784032+ragingo@users.noreply.github.com>
2026-08-26 23:22:20 +0900

    libcxxapi と libcxx をまとめてビルドするよう変更

--- scripts/build_libcxx.sh
+++ scripts/build_libcxx.sh
@@ -6,7 +6,7 @@
 # - INCREMENTAL_BUILD
 BUILD_MODE=CLEAN_BUILD
 
-LLVM_VERSION=19.1.4
+LLVM_VERSION=22.1.8
 LLVM_TAG=llvmorg-$LLVM_VERSION
 
 CC=clang
@@ -17,124 +17,68 @@ NEWLIB_INCLUDES="$(realpath ./lib/newlib_build/$TARGET_TRIPLE/include)"
 
 pushd ./lib
 
-if [ $BUILD_MODE == "CLONE_AND_BUILD" ]; then
+if [ "$BUILD_MODE" == "CLONE_AND_BUILD" ]; then
   rm -rf ./llvm
-  rm -rf ./llvm_libcxxabi_build
-  rm -rf ./llvm_libcxx_build
+  rm -rf ./llvm_runtimes_build
   git clone --depth=1 -b $LLVM_TAG https://github.com/llvm/llvm-project.git llvm
 
-  pushd llvm
-  git apply ../../scripts/llvm_libcxxabi_src_CMakeLists_disable-cxxabi-test-depends.patch
-  popd
-
-  mkdir llvm_libcxxabi_build
-  mkdir llvm_libcxx_build
+  mkdir -p llvm_runtimes_build
 fi
 
 CXX_FLAGS="\
   -nostdlibinc \
   -O2 \
   -D__ELF__ \
-  -D__GLIBC_USE\(...\)=0 \
   -D_GNU_SOURCE \
   -D_LDBL_EQ_DBL \
-  -D_LIBCPP_HAS_NO_MONOTONIC_CLOCK \
-  -D_LIBCPP_HAS_NO_THREADS \
   -D_POSIX_TIMERS \
-  -DDISABLE_THREADS \
+  -D_LIBCPP_PROVIDES_DEFAULT_RUNE_TABLE \
   -mcmodel=large \
 "
 
-#====================
-# libcxxabi build
-#====================
-pushd llvm_libcxxabi_build
-mkdir -p include
+#====================================
+# libcxx & libcxxabi Unified Build
+#====================================
+pushd llvm_runtimes_build
 
-if [ $BUILD_MODE == "CLEAN_BUILD" ]; then
-  rm cmake_install.cmake || true
-  rm CMakeCache.txt || true
-  rm ./lib/libc++abi.a || true
+if [ "$BUILD_MODE" == "CLEAN_BUILD" ]; then
+  rm -rf CMakeCache.txt CMakeFiles || true
 fi
 
-CXX_INCLUDES="\
-  -I$(realpath ../llvm/libcxx/include/c++/v1) \
-  -I$(realpath ../llvm/libcxx/include) \
-  -I/usr/include/c++/v1 \
-  -I$NEWLIB_INCLUDES \
-  -I/usr/include \
-"
-
-# NOTE:
-# - CMAKE_INSTALL_PREFIX を設定しても効果がない
 cmake -G Ninja \
+  -S ../llvm/runtimes \
+  -B . \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_C_COMPILER=$CC \
   -DCMAKE_C_COMPILER_TARGET=$TARGET_TRIPLE \
-  -DCMAKE_C_FLAGS="$CXX_INCLUDES $CXX_FLAGS" \
+  -DCMAKE_C_FLAGS="$CXX_FLAGS -I$NEWLIB_INCLUDES" \
   -DCMAKE_CXX_COMPILER=$CXX \
   -DCMAKE_CXX_COMPILER_TARGET=$TARGET_TRIPLE \
-  -DCMAKE_CXX_FLAGS="$CXX_INCLUDES $CXX_FLAGS -std=c++23" \
+  -DCMAKE_CXX_FLAGS="$CXX_FLAGS -I$NEWLIB_INCLUDES -std=c++23" \
   -DCMAKE_LINKER=$LD \
   -DCMAKE_TRY_COMPILE_TARGET_TYPE=STATIC_LIBRARY \
-  -DLIBCXXABI_ENABLE_EXCEPTIONS=False \
-  -DLIBCXXABI_ENABLE_SHARED=False \
-  -DLIBCXXABI_ENABLE_STATIC=True \
-  -DLIBCXXABI_ENABLE_THREADS=False \
-  -DLIBCXXABI_INCLUDE_TESTS=False \
-  -DLIBCXXABI_USE_LLVM_UNWINDER=OFF \
   -DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi" \
-  ../llvm/libcxxabi
+  -DLIBCXX_ENABLE_EXCEPTIONS=OFF \
+  -DLIBCXXABI_ENABLE_EXCEPTIONS=OFF \
+  -DLIBCXX_ENABLE_RTTI=OFF \
+  -DLIBCXX_ENABLE_SHARED=OFF \
+  -DLIBCXXABI_ENABLE_SHARED=OFF \
+  -DLIBCXX_ENABLE_STATIC=ON \
+  -DLIBCXXABI_ENABLE_STATIC=ON \
+  -DLIBCXX_ENABLE_THREADS=OFF \
+  -DLIBCXXABI_ENABLE_THREADS=OFF \
+  -DLIBCXX_HAS_THREAD_API=OFF \
+  -DLIBCXXABI_HAS_THREAD_API=OFF \
+  -DLIBCXX_ENABLE_LOCALIZATION=ON \
+  -DLIBCXX_ENABLE_FILESYSTEM=OFF \
+  -DLIBCXX_ENABLE_TIME_ZONE_DATABASE=OFF \
+  -DLIBCXX_ENABLE_MONOTONIC_CLOCK=OFF \
+  -DLIBCXX_INCLUDE_BENCHMARKS=OFF \
+  -DLIBCXX_INCLUDE_TESTS=OFF \
+  -DLIBCXXABI_INCLUDE_TESTS=OFF \
+  -DLIBCXXABI_USE_LLVM_UNWINDER=OFF
 
-ninja -j$(nproc)
-
-popd
-
-#====================
-# libcxx build
-#====================
-pushd llvm_libcxx_build
-
-if [ $BUILD_MODE == "CLEAN_BUILD" ]; then
-  rm cmake_install.cmake || true
-  rm CMakeCache.txt || true
-  rm ./lib/libc++.a || true
-  rm ./lib/libc++experimental.a || true
-fi
-
-CXX_ABI_DIR="$(realpath ../llvm_libcxxabi_build)"
-
-CXX_INCLUDES="\
-  -I$CXX_ABI_DIR/include/c++/v1 \
-  -I$CXX_ABI_DIR/include \
-  -I$(realpath ./include/c++/v1) \
-  -I$(realpath ./include) \
-  -I$NEWLIB_INCLUDES \
-"
-
-cmake -G Ninja \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_C_COMPILER=$CC \
-  -DCMAKE_C_COMPILER_TARGET=$TARGET_TRIPLE \
-  -DCMAKE_C_FLAGS="$CXX_INCLUDES $CXX_FLAGS" \
-  -DCMAKE_CXX_COMPILER=$CXX \
-  -DCMAKE_CXX_FLAGS="$CXX_INCLUDES $CXX_FLAGS -Wno-c++11-narrowing -std=c++23" \
-  -DCMAKE_CXX_COMPILER_TARGET=$TARGET_TRIPLE \
-  -DCMAKE_LINKER=$LD \
-  -DCMAKE_TRY_COMPILE_TARGET_TYPE=STATIC_LIBRARY \
-  -DLIBCXX_CXX_ABI=libcxxabi \
-  -DLIBCXX_ENABLE_EXCEPTIONS=False \
-  -DLIBCXX_ENABLE_FILESYSTEM=False \
-  -DLIBCXX_ENABLE_RTTI=False \
-  -DLIBCXX_ENABLE_SHARED=False \
-  -DLIBCXX_ENABLE_STATIC=True \
-  -DLIBCXX_ENABLE_THREADS=False \
-  -DLIBCXX_INCLUDE_BENCHMARKS=False \
-  -DLIBCXX_INCLUDE_TESTS=False \
-  -DLIBCXXABI_USE_LLVM_UNWINDER=OFF \
-  ../llvm/libcxx
-
-cmake --build . --target cxx_static -- -j$(nproc)
+ninja -j$(nproc) cxx cxxabi
 
 popd
 
