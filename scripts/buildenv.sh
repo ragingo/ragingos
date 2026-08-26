6d8b6c4454cf5a822ec087b263eb342062d679a2
ragingo <4784032+ragingo@users.noreply.github.com>
2026-08-26 23:24:33 +0900

    libcxxapi と libcxx をまとめてビルドするよう変更

--- scripts/buildenv.sh
+++ scripts/buildenv.sh
@@ -8,13 +8,11 @@ export AS=nasm
 export EDK2_HOME=$(realpath -m ./lib/edk2)
 export EFI_FILE_PATH=$EDK2_HOME/Build/RagingosLoaderX64/DEBUG_CLANG38/X64/Loader.efi
 export NEWLIB_DIR=$(realpath -m ./lib/newlib_build/x86_64-elf)
-export CXX_ABI_DIR=$(realpath -m ./lib/llvm_libcxxabi_build)
-export CXX_DIR=$(realpath -m ./lib/llvm_libcxx_build)
+export CXX_RUNTIMES_DIR="$(realpath -m ./lib/llvm_runtimes_build)"
 export FREETYPE_DIR=$(realpath -m ./lib/freetype_build)
 
 export CPPFLAGS="\
-  -I$CXX_ABI_DIR/include/c++/v1 \
-  -I$CXX_DIR/include/c++/v1 \
+  -I${CXX_RUNTIMES_DIR}/include/c++/v1 \
   -I$NEWLIB_DIR/include \
   -I$FREETYPE_DIR/include/freetype2 \
   -I$EDK2_HOME/MdePkg/Include \
@@ -24,12 +22,12 @@ export CPPFLAGS="\
   -D_GNU_SOURCE \
   -D_LDBL_EQ_DBL \
   -D_POSIX_TIMERS \
+  -D_LIBCPP_PROVIDES_DEFAULT_RUNE_TABLE \
   -DEFIAPI='__attribute__((ms_abi))'\
-"
+ "
 
 export LDFLAGS="\
-  -L$CXX_ABI_DIR/lib \
-  -L$CXX_DIR/lib \
+  -L${CXX_RUNTIMES_DIR}/lib \
   -L$NEWLIB_DIR/lib \
   -L$FREETYPE_DIR/lib\
 "
