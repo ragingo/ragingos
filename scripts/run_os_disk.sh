0e752ebf4f02a28de52429963ebab39def03ccd3
ragingo <4784032+ragingo@users.noreply.github.com>
2026-08-29 15:43:20 +0900

    Ubuntu26.04 (Arm64) で OS 起動できるよう諸々修正

--- scripts/run_os_disk.sh
+++ scripts/run_os_disk.sh
@@ -5,20 +5,13 @@ readonly OUTPUT_PATH=./build
 
 pushd $OUTPUT_PATH > /dev/null
 
-cp ../lib/edk2/Build/OvmfX64/DEBUG_GCC5/FV/OVMF.fd OVMF_SELF_BUILD.fd
-cp /usr/share/ovmf/OVMF.fd OVMF_Ubuntu24-apt.fd
+cp ../lib/edk2/Build/OvmfX64/DEBUG_CLANG38/FV/OVMF.fd OVMF_SELF_BUILD.fd
+cp /usr/share/ovmf/OVMF.fd OVMF_Ubuntu26-apt.fd
 
 popd > /dev/null
 
 # FIRMWARE=""
-FIRMWARE="-bios $OUTPUT_PATH/OVMF_Ubuntu22-apt.fd" # タスクバーは消える以外は問題なさそう
-# FIRMWARE="-bios $OUTPUT_PATH/OVMF_Ubuntu24-apt.fd" # 起動直後にOS停止
-# FIRMWARE="-bios $OUTPUT_PATH/OVMF_SELF_BUILD.fd" # 起動直後にOS停止
-# FIRMWARE="-bios $OUTPUT_PATH/DEBUGX64_OVMF.fd" # 起動直後にOS停止
-# FIRMWARE="\
-#   -drive if=pflash,format=raw,readonly=on,file=$OUTPUT_PATH/OSBOOK_OVMF_CODE.fd \
-#   -drive if=pflash,format=raw,file=$OUTPUT_PATH/OSBOOK_OVMF_VARS.fd
-# " # タスクバーは消える以外は問題なさそう
+FIRMWARE="-bios $OUTPUT_PATH/OVMF_Ubuntu26-apt.fd"
 
 QEMU_OPTS=""
 # QEMU_OPTS="-gdb tcp::12345 -S"
